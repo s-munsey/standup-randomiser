91f6a5d2ca675addbe7343292a282a2ec348d255
Shaun Munsey <shaunmunsey@alphafx.co.uk>
2023-03-10 18:53:29 +0000

    namebadge

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import NameBadge from "./components/NameBadge";
+
+const team = [
+  "AP",
+  "AG",
+  "AD",
+  "GJ",
+  "KT",
+  "LB",
+  "LE",
+  "RL",
+  "RD",
+  "SM",
+  "TB",
+  "WD",
+  "YS",
+];
+
+const shuffled = shuffle(team);
 
 function App() {
+  const [next, setNext] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NameBadge name={next} />
+      <button onClick={() => setNext(shuffled.pop())}>Click me</button>
+      <h1>Team</h1>
+      {}
     </div>
   );
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,
+    randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex !== 0) {
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
 }
 
 export default App;
